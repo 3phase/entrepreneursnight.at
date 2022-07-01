9247f7378b19b170d90e81aad4a73e3b065c2edf
3phase <pbozhinow@gmail.com>
2022-07-01 12:37:42 +0300

    Changes

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ const express = require('express');
 const app = express();
 // app.use(requireHTTPS);
 
-app.use(express.static('./dist'));
+app.use(express.static('./dist/entrepreneursnight'));
 
 app.get('/*', function (req, res) {
-    res.sendFile('index.html', { root: 'dist' }
+    res.sendFile('index.html', { root: 'dist/entrepreneursnight' }
     );
 });
 
